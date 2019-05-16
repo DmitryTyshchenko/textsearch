a33d0438f2116446d930f94fff985cfb69f2d624
Dmytro Tyshchenko <dmytro.tyshchenko@globallogic.com>
2019-05-16 18:14:37 +0300

    TermsVectorBuilder

--- src/main/java/ztysdmy/textsearch/model/TermsVector.java
+++ src/main/java/ztysdmy/textsearch/model/TermsVector.java
@@ -1,9 +1,10 @@
 package ztysdmy.textsearch.model;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class TermsVector {
 
@@ -36,8 +37,8 @@ public class TermsVector {
 	}
 
 	/**
-	 * Returns distance of This to Input TermsVector By
-	 * default calculates distance between two TermsVector as cosine
+	 * Returns distance of This to Input TermsVector By default calculates distance
+	 * between two TermsVector by using Tanimoto metric
 	 * 
 	 * @param input
 	 * @return
@@ -47,7 +48,8 @@ public class TermsVector {
 	}
 
 	/**
-	 * Return distance of This to Input TermsVector 
+	 * Return distance of This to Input TermsVector
+	 * 
 	 * @param input
 	 * @param strategy - Strategy of distance calculation
 	 * @return
@@ -57,10 +59,25 @@ public class TermsVector {
 		return strategy.apply(this, input);
 	}
 
+	//Tanimoto Metric implementation
 	private static final BiFunction<TermsVector, TermsVector, Double> defaultDistanceStratigy() {
 
 		return (tV1, tV2) -> {
-			return 0.d;
+
+			var x = tV1.terms.keySet().size();
+			var y = tV2.terms.keySet().size();
+
+			Set<String> termVector1 = new HashSet<>();
+			termVector1.addAll(tV1.terms.keySet());
+
+			Set<String> termVector2 = new HashSet<>();
+			termVector2.addAll(tV2.terms.keySet());
+
+			termVector1.retainAll(termVector2);
+
+			var common = termVector1.size();
+
+			return (1.d - ((x + y - 2 * common / x + y - common) * 1.d));
 		};
 	}
 
