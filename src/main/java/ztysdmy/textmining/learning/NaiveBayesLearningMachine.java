5559fd235015947b880a918f311e2d10a531b882
Dmytro Tyshchenko <dmytro.tyshchenko@globallogic.com>
2019-08-14 16:32:45 +0300

    Naive Bayes Learning Machine

--- src/main/java/ztysdmy/textmining/learning/NaiveBayesLearningMachine.java
+++ src/main/java/ztysdmy/textmining/learning/NaiveBayesLearningMachine.java
@@ -1,9 +1,24 @@
 package ztysdmy.textmining.learning;
 
 import ztysdmy.textmining.classifier.Classifier;
+import ztysdmy.textmining.repository.FactsRepository;
 
 public class NaiveBayesLearningMachine<T> implements Supervized<T> {
 
+	private FactsRepository<T> factsRepository;
+	private int complexity;
+	
+	public NaiveBayesLearningMachine(FactsRepository<T> factsRepository) {
+		
+		this(factsRepository, 1);
+	}
+	
+    public NaiveBayesLearningMachine(FactsRepository<T> factsRepository, int complexity) {
+		
+		this.factsRepository = factsRepository;
+		this.complexity = complexity;
+	}
+	
 	@Override
 	public Classifier<T> build() {
 		// TODO Auto-generated method stub
