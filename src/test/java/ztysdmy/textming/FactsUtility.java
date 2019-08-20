c3334df729a73fe770789a14109f57b5b22a3367
Dmytro Tyshchenko <dmytro.tyshchenko@globallogic.com>
2019-08-20 13:38:07 +0300

    some refactoring in tests

--- src/test/java/ztysdmy/textming/FactsUtility.java
+++ src/test/java/ztysdmy/textming/FactsUtility.java
@@ -1,66 +1,73 @@
 package ztysdmy.textming;
 
+import static ztysdmy.functional.tailcall.TailCallUtility.call;
+import static ztysdmy.functional.tailcall.TailCallUtility.done;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import ztysdmy.functional.tailcall.TailCall;
 import ztysdmy.textmining.model.Fact;
 import ztysdmy.textmining.model.Target;
 
 public class FactsUtility {
 
-	private FactsUtility() {}
-		
+	private FactsUtility() {
+	}
+
 	static Function<Integer, Function<Integer, Target<String>>> chooseTarget() {
-		return a-> b-> {
-			var toEval = new IF<Target<String>>(()->a.equals(b), ()->createTarget("classA"));
-			return match(toEval, ()->createTarget("classB"));
+		return a -> b -> {
+			var toEval = new IF<Target<String>>(() -> a.equals(b), () -> createTarget("classA"));
+			return match(toEval, () -> createTarget("classB"));
 		};
 	}
-		
-	private static Function<Target<String>, Fact<String>> setTargetToFact = x->{
+
+	private static Function<Target<String>, Fact<String>> setTargetToFact = x -> {
 		var fact = new Fact<String>("test", x);
 		return fact;
 	};
-	
+
 	public static Supplier<Collection<Fact<String>>> factsSupplier() {
-		return ()->{
+		return () -> {
 
 			ArrayList<Fact<String>> collector = new ArrayList<>();
-	
-			//code below is no functional but let it be pragmatics
-			for (int i=0;i<4;i++) {
-				collector.add(chooseTarget().apply(1).andThen(setTargetToFact).apply(i));
-			}
-			
-			return collector;
+			var result =  collect(0, collector).invoke();
+			return result;
 		};
 	}
-	
+
+	private static TailCall<Collection<Fact<String>>> collect(int i, ArrayList<Fact<String>> collector) {
+		collector.add(chooseTarget().apply(1).andThen(setTargetToFact).apply(i));
+		if (i == 4) {
+			return done(collector);
+		}
+		return call(() -> collect(i+1, collector));
+	}
+
 	private static Target<String> createTarget(String value) {
 		return new Target<String>(value);
 	}
-	
-	
+
 	private static class IF<T> {
-		
+
 		Supplier<Boolean> predicate;
 		Supplier<T> action;
-		
+
 		IF(Supplier<Boolean> predicate, Supplier<T> action) {
 			this.predicate = predicate;
 			this.action = action;
 		}
 	}
-	
+
 	private static <T> T match(IF<T> a, Supplier<T> defaultAction) {
-		
+
 		if (a.predicate.get().equals(Boolean.TRUE)) {
 			return a.action.get();
 		}
-		
+
 		return defaultAction.get();
 	}
-	
+
 }
