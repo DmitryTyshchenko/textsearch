6b8a23b9e73ab6aec83b9e949ab593e345caf081
Dmytro Tyshchenko <dmytro.tyshchenko@globallogic.com>
2019-09-30 14:23:03 +0300

    adding comments

--- src/main/java/ztysdmy/textmining/classifier/Sigmoid.java
+++ src/main/java/ztysdmy/textmining/classifier/Sigmoid.java
@@ -6,6 +6,8 @@ public class Sigmoid implements Function<Double, Double> {
 
 	@Override
 	public Double apply(Double t) {
+		//http://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+		// stable sigmoid
 		return 1/(1 + Math.exp(t>=0.d?negativeValue(t):t));
 	}
 
